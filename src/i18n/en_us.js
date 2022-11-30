2ae86f7cae58a5808c9f56254b07ae8e9d436858
yuanx <1220998011@qq.com>
2022-11-30 23:30:25 +0800

    feat: new i18n

---
+++ src/i18n/en_us.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    "Share Directory": "Share Directory",
+    Change: "Change",
+    Select: "Select",
+    Directory: "Directory",
+    Port: "Port",
+    "Share Clipborad": "Share Clipborad",
+    "Rective files to directory": "Rective files to directory",
+    "Public IP": "Public IP",
+    Username: "Username",
+    Password: "Password",
+    "Start Service": "Start Service",
+    "End Service": "End Service",
+    Optional: "Optional",
+    Copied: "Copied",
+    "Please select a directory!": "Please select a directory!",
+  },
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.jsx";
 import "./index.css";
+import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
