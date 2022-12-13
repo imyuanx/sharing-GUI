cffe0f4cb36a9d969bbea78ff92f9c07934ba7cc
yuanx <1220998011@qq.com>
2022-12-13 10:30:03 +0800

    chore: Update version number

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   packagerConfig: {
-    appVersion: "1.1.0",
+    appVersion: "1.2.0",
     name: "Sharing GUI",
     appCopyright: "yuanx(yuanx.me@qq.com)",
     icon: "./assets/logo-bg-512x512",
