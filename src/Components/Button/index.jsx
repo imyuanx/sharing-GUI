77de81ef6b47e4800c17801f088eab01b72a1910
yuanx <yuanx.me@qq.com>
2023-09-14 12:02:32 +0800

    refactor: refactor svg

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,3 +1,4 @@
+import { ReactComponent as LoadingSvg } from "../../icons/loading.svg";
 import "./index.css";
 
 function Button({ ghost, size, className, children, isLoding, ...args }) {
@@ -9,40 +10,7 @@ function Button({ ghost, size, className, children, isLoding, ...args }) {
       {...args}
     >
       {children}
-      {isLoding && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-          className="loading-icon"
-        >
-          <path
-            fill="none"
-            stroke="currentColor"
-            strokeDasharray="15"
-            strokeDashoffset="15"
-            strokeLinecap="round"
-            strokeWidth="2"
-            d="M12 3C16.9706 3 21 7.02944 21 12"
-          >
-            <animate
-              fill="freeze"
-              attributeName="stroke-dashoffset"
-              dur="0.3s"
-              values="15;0"
-            />
-            <animateTransform
-              attributeName="transform"
-              dur="1s"
-              repeatCount="indefinite"
-              type="rotate"
-              values="0 12 12;360 12 12"
-            />
-          </path>
-        </svg>
-      )}
+      {isLoding && <LoadingSvg className="loading-icon" />}
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import electron from "vite-plugin-electron";
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+import svgr from "vite-plugin-svgr";
+
 const require = createRequire(import.meta.url); // construct the require method
 const pkg = require("./package.json");
 console.log(electron);
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV !== 'development' ? './' : '/',
+  base: process.env.NODE_ENV !== "development" ? "./" : "/",
   build: {
-    outDir: 'vite-build',
+    outDir: "vite-build",
   },
   plugins: [
     react(),
@@ -18,6 +20,7 @@ export default defineConfig({
         entry: "./main.cjs",
       },
     }),
+    svgr(),
   ],
   server: {
     host: pkg.env.SERVER_HOST,
