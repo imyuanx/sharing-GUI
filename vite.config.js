ee330a050a1a6059b43bd5d1a157ded30f4ad655
yuanx <yuanx.me@qq.com>
2023-10-09 23:20:09 +0800

    style: clean log

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { resolve } from "path"; // 导入 path 模块，帮助我们解析路径
 
 const require = createRequire(import.meta.url); // construct the require method
 const pkg = require("./package.json");
-console.log(electron);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV !== "development" ? "./" : "/",
